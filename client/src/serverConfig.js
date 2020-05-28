5370fc3096c433f9b5d17de6e8fb1486584b431c
PutthidaSR <psamrith@uw.edu>
2020-05-28 14:11:21 -0700

    updated Heroku IP

--- client/src/serverConfig.js
+++ client/src/serverConfig.js
@@ -1,1 +1,2 @@
-export const SERVER_IP_ADDRESS = "http://localhost:9000";
+//export const SERVER_IP_ADDRESS = "http://localhost:9000";
+export const SERVER_IP_ADDRESS = 'https://vast-bastion-59857.herokuapp.com';
--- server/app.js
+++ server/app.js
@@ -54,6 +54,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log('Failed!!!')
   next(createError(404));
 });
 
